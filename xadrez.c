41216dd89634553345018dbbe1828198ed5c0f2f
GabrielDuraky <gabrielmourabatistagmb10@gmail.com>
2025-09-01 20:06:12 +0000

    Adição de estruturas aninhadas

--- xadrez.c
+++ xadrez.c
@@ -3,31 +3,60 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    int torre = 1, bispo, rainha = 1; // declaração das peças e inicialização das peças torre e rainha
+    int torre = 1, bispo_diagonal = 1, rainha;
+    int cavalo, movimento_cavalo; // declaração das peças e inicialização das peças torre e rainha
 
     // Implementação de Movimentação do Bispo
-    for (bispo = 1; bispo <= 5; bispo++) // estrutura for inicializando bispo em 1 casa, condicional e incremento
+    printf("Movimentação do bispo.\n");
+    while (bispo_diagonal <= 5)
     {
-        printf("Bispo andou %d casas para cima/direita.\n", bispo); // saida de movimentos do bispo
+        int bispo = 1;
+        while (bispo == 1)
+        {
+            printf("Bispo andou %d casa para cima e para direita\n", bispo_diagonal);
+            bispo++;
+        }
+        bispo_diagonal++;
+        
     }
+    printf("\n");
+    
+   
 
     // Implementação de Movimentação da Torre
+    printf("Movimentação da torre.\n");
     do
     {
         printf("Torre andou %d casas par direita\n", torre); // saída de movimentos da torre
         torre++; // incremento para prevenção de loop infinito
     } while (torre <= 5); // condicional da torre
+    printf("\n");
+    
 
     // Implementação de Movimentação da Rainha
-    while (rainha <=8) // condicional a rainha
+    printf("Movimentação da rainha.\n");
+    for (rainha = 1; rainha <= 8; rainha++)
     {
-        printf("Rainha andou %d casas para esquerda.\n", rainha); // saída de movimentos da rainha
-        rainha++; // incremento para prevenção de loop infinito
+        printf("Rainha andou %d casas para esquerda.\n", rainha);
+    }
+    printf("\n");
+    
+
+    // Implementação de Movimentação do Cavalo
+    printf("Movimentação do cavalo.\n");
+
+    for (movimento_cavalo = 1; movimento_cavalo <= 1; movimento_cavalo++)
+    {
+        for (cavalo = 1; cavalo <= 2; cavalo++)
+        {
+            printf("Cavalo andou %d casa para baixo.\n", cavalo);
+        }
+        
+        printf("Cavalo andou %d casa para esquerda.\n", movimento_cavalo);
+        printf("\n");
     }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
